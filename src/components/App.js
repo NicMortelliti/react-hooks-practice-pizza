3558eb45269e4137efc483b48e4e03dd10c0e5ba
NicMortelliti <npmortelliti@gmail.com>
2022-04-11 16:21:12 -0700

    Fetch pizza data from API

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
+const API = "http://localhost:3001/pizzas";
+
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  // Initial collection of pizzas
+  useEffect(() => {
+    fetch(API)
+      .then(r => r.json())
+      .then(setPizzas);
+  }, []);
+
   return (
     <>
       <Header />
